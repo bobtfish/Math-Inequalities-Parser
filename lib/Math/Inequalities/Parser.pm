8937677105b724a17a5442641fe5246ffbd41feb
Tomas Doran <bobtfish@bobtfish.net>
2011-02-16 19:51:14 +0000

    Skeleton

---
+++ lib/Math/Inequalities/Parser.pm
@@ -0,0 +1,26 @@
+package Math::Inequalities::Parser;
+use Moose;
+use namespace::autoclean;
+
+1;
+
+=head1 NAME
+
+Math::Inequalities::Parser - 
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 BUGS
+
+=head1 AUTHOR
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 the above author(s).
+
+This sofware is free software, and is licensed under the same terms as perl itself.
+
+=cut
+
